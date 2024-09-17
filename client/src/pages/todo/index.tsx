d736a227e4a19971e37c563e0cc31583b77f6f82
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-09-17 13:45:02 +0900

    style: 불필요한 요소 삭제

--- client/src/pages/todo/index.tsx
+++ client/src/pages/todo/index.tsx
@@ -1,13 +1,7 @@
-import Form from '../../components/common/Form';
-
 export default function Todo() {
   return (
     <div>
       <div>todo</div>
-      <Form
-        onSubmit={inputs => console.log(inputs)}
-        placeholders={['Name', 'Email', 'Password']}
-      />
     </div>
   );
 }
