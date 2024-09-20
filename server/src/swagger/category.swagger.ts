bd839b5c2c095958bfe9416024aa0d783b20567d
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-09-20 12:12:52 +0900

    fix: swagger 수정

--- server/src/swagger/category.swagger.ts
+++ server/src/swagger/category.swagger.ts
@@ -1,10 +1,45 @@
 /**
  * @swagger
  * tags:
- *   name: Categories
+ *   name: Category
  *   description: 카테고리 관련 API
  *
  * /api/category:
+ *   get:
+ *     summary: 전체 카테고리 조회
+ *     tags: [Category]
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: true
+ *                   description: 성공 여부
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                         description: 카테고리 ID
+ *                       name:
+ *                         type: string
+ *                         example: 일상
+ *                         description: 카테고리 이름
+ *                       color:
+ *                         type: string
+ *                         example: #d3d3d3
+ *                         description: 카테고리 색상
+ *       500:
+ *         $ref: '#/components/responses/ServerError'
+ * 
  *   post:
  *     summary: 카테고리 추가
  *     tags: [Category]
@@ -25,148 +60,32 @@
  *                 description: 카테고리 색상 (hex)
  *     responses:
  *       200:
- *         description: OK
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 카테고리를 등록했습니다.
- *                   description: 성공 메시지
+ *         $ref: '#/components/responses/SuccessResponse'
  *       404:
- *         description: 존재하지 않는 데이터입니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 존재하지 않는 데이터입니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/NotFoundError'
  *       500:
- *         description: 서버 오류가 발생했습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 서버 오류가 발생했습니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/ServerError'
  *
- * /api/category:
- *   get:
- *     summary: 전체 카테고리 조회
- *     tags: [Category]
- *     responses:
- *       200:
- *         description: OK
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                   description: 성공 여부
- *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: integer
- *                       example: 1
- *                       description: 카테고리 ID
- *                     name:
- *                       type: string
- *                       example: 일상
- *                       description: 카테고리 이름
- *                     color:
- *                       type: string
- *                       example: #d3d3d3
- *                       description: 카테고리 색상
- *       500:
- *         description: 서버 오류가 발생했습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 서버 오류가 발생했습니다.
- *                   description: 에러 메세지
+
  *
- * /api/category/:id:
+ * /api/category/{id}:
  *   delete:
  *     summary: 카테고리 삭제
  *     tags: [Category]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: 카테고리 ID
  *     responses:
  *       200:
- *         description: OK
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 카테고리가 삭제되었습니다.
- *                   description: 성공 메세지
+ *         $ref: '#/components/responses/SuccessResponse'
  *       404:
- *         description: 일치하는 데이터가 없습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 일치하는 데이터가 없습니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/NotFoundError'
  *       500:
- *         description: 서버 오류가 발생했습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 서버 오류가 발생했습니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/ServerError'
  *
  * /api/category/percentage:
  *   get:
@@ -185,33 +104,70 @@
  *                   example: true
  *                   description: 성공 여부
  *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: integer
- *                       example: 1
- *                       description: 카테고리 ID
- *                     name:
- *                       type: string
- *                       example: 일상
- *                       description: 카테고리 이름
- *                     todoCount:
- *                       type: number
- *                       example: 0
- *                       description: 카테고리 별 투두 수
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         example: 1
+ *                         description: 카테고리 ID
+ *                       name:
+ *                         type: string
+ *                         example: 일상
+ *                         description: 카테고리 이름
+ *                       todoCount:
+ *                         type: number
+ *                         example: 0
+ *                         description: 카테고리 별 투두 수
  *       500:
- *         description: 서버 오류가 발생했습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 서버 오류가 발생했습니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/ServerError'
+ *
+ * components:
+ *   responses:
+ *     SuccessResponse:
+ *       description: OK
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               success:
+ *                 type: boolean
+ *                 example: true
+ *                 description: 성공 여부
+ *               message:
+ *                 type: string
+ *                 example: 작업이 성공적으로 완료되었습니다.
+ *                 description: 성공 메시지
+ *     NotFoundError:
+ *       description: 존재하지 않는 데이터입니다.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               success:
+ *                 type: boolean
+ *                 example: false
+ *                 description: 성공 여부
+ *               message:
+ *                 type: string
+ *                 example: 존재하지 않는 데이터입니다.
+ *                 description: 에러 메세지
+ *     ServerError:
+ *       description: 서버 오류가 발생했습니다.
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               success:
+ *                 type: boolean
+ *                 example: false
+ *                 description: 성공 여부
+ *               message:
+ *                 type: string
+ *                 example: 서버 오류가 발생했습니다.
+ *                 description: 에러 메세지
  */
--- server/src/swagger/reminder.swagger.ts
+++ server/src/swagger/reminder.swagger.ts
@@ -4,6 +4,63 @@
  *   name: Reminder
  *   description: 알림 관련 API
  *
+ * components:
+ *   schemas:
+ *     Reminder:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           example: 1
+ *           description: 알림 ID
+ *         todo_id:
+ *           type: number
+ *           example: 1
+ *           description: 투두 ID
+ *         reminder_time:
+ *           type: string
+ *           example: 2024-09-19 18:00
+ *           description: 알림 시간
+ *         message:
+ *           type: string
+ *           example: Hello World
+ *           description: 알림 메세지
+ *         created_at:
+ *           type: string
+ *           example: 2024-09-19 18:00
+ *           description: 알림 생성 일자
+ *
+ *   responses:
+ *     SuccessResponse:
+ *       description: OK
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               success:
+ *                 type: boolean
+ *                 example: true
+ *                 description: 성공 여부
+ *               message:
+ *                 type: string
+ *                 description: 성공 메시지
+ *
+ *     ErrorResponse:
+ *       description: 에러 응답
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               success:
+ *                 type: boolean
+ *                 example: false
+ *                 description: 성공 여부
+ *               message:
+ *                 type: string
+ *                 description: 에러 메세지
+ *
  * /api/reminder:
  *   get:
  *     summary: 전체 알림 조회
@@ -21,47 +78,14 @@
  *                   example: true
  *                   description: 성공 여부
  *                 data:
- *                   type: object
- *                   properties:
- *                     id:
- *                       type: integer
- *                       example: 1
- *                       description: 카테고리 ID
- *                     todo_id:
- *                       type: number
- *                       example: 1
- *                       description: 투두 ID
- *                     reminder_time:
- *                       type: string
- *                       example: 2024-09-19 18:00
- *                       description: 알림 시간
- *                     message:
- *                       type: string
- *                       example: Hello World
- *                       description: 알림 메세지
- *                     created_at:
- *                       type: string
- *                       example: 2024-09-19 18:00
- *                       description: 알림 생성 일자
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Reminder'
  *       500:
- *         description: 서버 오류가 발생했습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 서버 오류가 발생했습니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/ErrorResponse'
  *
- * /api/reminder:
  *   post:
- *     summary: 알림 추가 추가
+ *     summary: 알림 추가
  *     tags: [Reminder]
  *     requestBody:
  *       required: true
@@ -84,48 +108,9 @@
  *                 description: 알림 메세지
  *     responses:
  *       200:
- *         description: OK
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: true
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 알림이 등록되었습니다.
- *                   description: 성공 메시지
+ *         $ref: '#/components/responses/SuccessResponse'
  *       404:
- *         description: 이미 존재하는 데이터입니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 이미 존재하는 데이터입니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/ErrorResponse'
  *       500:
- *         description: 서버 오류가 발생했습니다.
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 success:
- *                   type: boolean
- *                   example: false
- *                   description: 성공 여부
- *                 message:
- *                   type: string
- *                   example: 서버 오류가 발생했습니다.
- *                   description: 에러 메세지
+ *         $ref: '#/components/responses/ErrorResponse'
  */
