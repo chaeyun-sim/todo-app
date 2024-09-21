07c5efadf85f79627a844475538a31e2db359ed9
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-09-21 10:41:05 +0900

    feat: 푸터에 조건 추가 및 pathname 별 height 설정

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -70,8 +70,15 @@ export default function Layout({ children }: PropsWithChildren) {
             )}
           </header>
         )}
-        <div className={style.children}>{children}</div>
-        <footer className={style.footer}>© 2024 chaeyun-sim.</footer>
+        <div
+          className={style.children}
+          style={{
+            height: pathname === '/my' ? 'calc(100% - 60px - 60px - 10px)' : 'calc(100% - 60px)',
+          }}
+        >
+          {children}
+        </div>
+        {pathname === '/my' && <footer className={style.footer}>© 2024 chaeyun-sim.</footer>}
       </div>
       {isOpen && (
         <NotificationModal
