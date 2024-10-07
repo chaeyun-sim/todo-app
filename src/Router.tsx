8f3856592a8c66ab56a5616e73bc9c48b83b9dfc
Simune <111689342+chaeyun-sim@users.noreply.github.com>
2024-10-07 22:08:35 +0900

    feat: AuthLayout 구현 및 회원가입, 로그인 페이지 컴포넌트화

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
+import AuthLayout from './components/layout/AuthLayout';
 
 const Todo = lazy(() => import('./pages/todo'));
 const Login = lazy(() => import('./pages/login'));
@@ -8,25 +9,27 @@ const My = lazy(() => import('./pages/my'));
 
 export default function Router() {
   return (
-    <Routes>
-      <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
         <Route
           path='/'
           element={<Todo />}
         />
-        <Route
-          path='/login'
-          element={<Login />}
-        />
-        <Route
-          path='/join'
-          element={<Join />}
-        />
+        <Route element={<AuthLayout />}>
+          <Route
+            path='/login'
+            element={<Login />}
+          />
+          <Route
+            path='/join'
+            element={<Join />}
+          />
+        </Route>
         <Route
           path='/my'
           element={<My />}
         />
-      </Suspense>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 }
---
+++ src/components/layout/AuthLayout/index.tsx
@@ -0,0 +1,51 @@
+import { Link, Outlet, useOutletContext } from 'react-router-dom';
+import logo from '../../../assets/logo-big.svg';
+import style from './index.module.css';
+import { useState } from 'react';
+
+type ContextType = {
+  setSubmitHandler: (handler: (e: React.FormEvent) => void) => void;
+};
+
+export function useAuth() {
+  return useOutletContext<ContextType>();
+}
+
+export default function AuthLayout() {
+  const [submitHandler, setSubmitHandler] = useState<(e: React.FormEvent) => void>(
+    () => (e: React.FormEvent) => {
+      e.preventDefault();
+    }
+  );
+
+  const isLogin = window.location.pathname === '/login';
+
+  return (
+    <div className={style.container}>
+      <img
+        src={logo}
+        alt='로고'
+      />
+      <form onSubmit={submitHandler}>
+        <Outlet context={{ setSubmitHandler }} />
+        <div className={style.submit_box}>
+          <button
+            type='submit'
+            className={style.submit_btn}
+          >
+            {isLogin ? '로그인' : '회원가입'}
+          </button>
+          <p className={style.more_text}>
+            또는{' '}
+            <Link
+              className={style.do_else}
+              to={isLogin ? '/join' : '/login'}
+            >
+              {isLogin ? '회원가입하기' : '로그인하기'}
+            </Link>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+}
